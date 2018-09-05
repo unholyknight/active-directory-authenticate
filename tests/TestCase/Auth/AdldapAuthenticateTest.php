82f3a2b9c455bee7aee76651df9240a752633999
Alysson Azevedo <alysson@rxsaude.com.br>
2018-09-04 22:32:04 -0300

    Update composer and bootstrap the tests.

---
+++ tests/TestCase/Auth/AdldapAuthenticateTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ActiveDirectoryAuthenticate\Test\TestCase\Auth;
+
+
+use Cake\TestSuite\TestCase;
+
+class AdldapAuthenticateTest extends TestCase
+{
+    /**
+     * setup.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * testConfig.
+     *
+     * @return void
+     */
+    public function testConfig()
+    {
+        $this->markTestIncomplete();
+    }
+}
